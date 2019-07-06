cb3fb342b8b17550ba0f17d8771900c227f5588b
申昇祐 <shinsw@ais-info.co.jp>
2019-07-06 10:22:04 +0900

    データ入力処理追加

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -4,16 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\Rank;
+use Log;
 
 class APIController extends BaseController
 {
     public function getDatas() {
-        $dataCollection = Rank::all();
-        $dataArr = $dataCollection->toArray();
-        echo json_encode($dataArr);
+        $result = array();
+        try {
+            $dataCollection = Rank::all();
+            $result = $dataCollection->toArray();
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            $result = false;
+        }
+        return json_encode($result);
     }
 
-    public function putData() {
+    public function putData(\Request $request) {
+        $result = true;
+        try {
+            $name = $request->name;
+            $score = $request->score;
 
+            $query = new Rank();
+            $query->name = $name;
+            $query->score = $score;
+            $query->save();
+        } catch (\Exception $e) {
+            Log::error($e->getMessage());
+            $result = false;
+        }
+        return $result;
     }
 }
